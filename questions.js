fd6301949436427998a742fba63d94e9b74aad9c
lukerpace <lukerpace@gmail.com>
2024-03-12 15:53:38 -0500

    wrote html and added styling in css

---
+++ questions.js
@@ -0,0 +1,159 @@
+
+// const myQuestions = [
+//     {
+//         question: "The white spot behind an orca's dorsal fin is called what?",
+//         answers: {
+//             a: "White spot",
+//             b: "Dorsal patch",
+//             c: "Saddle patch",
+//             d: "Birthmark"
+//         },
+//         correctAnswer: "c"
+    // },
+    // {
+    //     question: "Orcas are classified as?",
+    //     answers: {
+    //         a: "Whale",
+    //         b: "Dolphin",
+    //         c: "Fish",
+    //         d: "Human"
+    //     },
+    //     correctAnswer: "b"
+    // },
+    // {
+    //     question: "sea otters have how many hairs per squar inch of their body?",
+    //     answers: {
+    //         a: "1000",
+    //         b: "10,000",
+    //         c: "100,000",
+    //         d: "1,000,000"
+    //     },
+    //     correctAnswer: "d"
+    // },
+    // {
+    //     question: "the largest whale in the world is?",
+    //     answers: {
+    //         a: "Sperm Whale",
+    //         b: "Blue Whale",
+    //         c: "Fin Whale",
+    //         d: "Whale Shark"
+    //     },
+    //     correctAnswer: "b"
+    // },
+    // {
+    //     question: "How long can blue whales hold their breath",
+    //     answers: {
+    //         a: "90 Minutes",
+    //         b: "1 Hour",
+    //         c: "30 Minutes",
+    //         d: "Whales dont breath air"
+    //     },
+    //     correctAnswer: "a"
+    // },
+    // {
+    //     question: "Dall's porpoise can swim how fast?",
+    //     answers: {
+    //         a: "15 mph",
+    //         b: "50mph",
+    //         c: "35 mph",
+    //         d: "5mph"
+    //     },
+    //     correctAnswer: "c"
+    // },
+//     {
+//         question: "how long do male orcas stay with their mothers",
+//         answers: {
+//             a: "2 years",
+//             b: "until they find a mate",
+//             c: "Their whole lives",
+//             d: "10 years"
+//         },
+//         correctAnswer: "c"
+//     },
+// ]
+
+let questions = [
+    {
+        numb: 1,
+        question: "The white spot behind an orca's dorsal fin is called what?",
+        answer: "Saddle patch",
+        options: [
+            "White spot",
+            "Dorsal patch",
+            "Saddle patch",
+            "Birthmark"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Orcas are classified as?",
+        answer: "Dolphin",
+        options: [
+            "Whale",
+            "Dolphin",
+            "Fish",
+            "Human"
+        ]
+    },
+    {
+        numb: 3,
+        question: "sea otters have how many hairs per squar inch of their body?",
+        answer: "1,000,000",
+        options: [
+            "1000",
+            "10,000",
+            "100,000",
+            "1,000,000"
+        ]
+    },
+    {
+        numb: 4,
+        question:  "the largest whale in the world is?",
+        answer: "Blue Whale",
+        options: [
+            "Sperm Whale",
+            "Blue Whale",
+            "Fin Whale",
+            "Whale Shark"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How long can blue whales hold their breath",
+        answer: "90 Minutes",
+        options: [
+            "90 Minutes",
+            "1 Hour",
+            "30 Minutes",
+            "Whales dont breath air"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Dall's porpoise can swim how fast?",
+        answer: "35 mph",
+        options: [
+            "15 mph",
+            "50mph",
+            "35 mph",
+            "5mph"
+        ]
+    },
+    {
+        numb: 7,
+        question: "how long do male orcas stay with their mothers",
+        answer: "Their whole lives",
+        options: [
+            "2 years",
+            "Until they find a mate",
+            "Their whole lives",
+            "10 years"
+        ]
+    },
+]
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,92 +1,15 @@
-const quizContainer = document.getElementById('quiz')
-const resultsContainer = document.getElementById('results')
-const submitButtom =document.getElementById('submit')
-
-const myQuestions = [
-    {
-        question: "The white spot behind an orcas dorsal fin is called what?",
-        answers: {
-            a: "White spot",
-            b: "Dorsal patch",
-            c: "Saddle patch",
-            d: "Birthmark"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Orcas are classified as?",
-        answers: {
-            a: "Whale",
-            b: "Dolphin",
-            c: "Fish",
-            d: "Human"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "sea otters have how many hairs per squar inch of their body?",
-        answers: {
-            a: "1000",
-            b: "10,000",
-            c: "100,000",
-            d: "1,000,000"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "the largest whale in the world is?",
-        answers: {
-            a: "Sperm Whale",
-            b: "Blue Whale",
-            c: "Fin Whale",
-            d: "Whale Shark"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "How long can blue whales hold their breath",
-        answers: {
-            a: "90 Minutes",
-            b: "1 Hour",
-            c: "30 Minutes",
-            d: "Whales dont breath air"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "Dall's porpoise can swim how fast?",
-        answers: {
-            a: "15 mph",
-            b: "50mph",
-            c: "35 mph",
-            d: "5mph"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "how long do male orcas stay with their mothers",
-        answers: {
-            a: "2 years",
-            b: "until they find a mate",
-            c: "forever",
-            d: "10 years"
-        },
-        correctAnswer: "c"
-    },
-]
+const start_btn = document.querySelector(".start_btn");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list"); 
+const time_line = document.querySelector(".header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
 
 
-
-function buildQuiz(){}
-
-function showResults(){}
-
-buildQuiz()
-
-submitButtom.addEventlistener('click', showResults)
-
-
-
-
-
-
+start_btn.onclick = ()=>{
+  info_box.classList.add("activeInfo");
+}
